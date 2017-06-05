5d559ccde7704d7a7ad10e76d94932a0acf6accd
Joe Oakes <joe.oakes@gmail.com>
2017-06-04 22:53:01 -0400

    Add by Joe

--- app/src/main/java/edu/psu/ist402/activitytest/MainActivity.java
+++ app/src/main/java/edu/psu/ist402/activitytest/MainActivity.java
@@ -2,6 +2,7 @@ package edu.psu.ist402.activitytest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,29 +16,34 @@ public class MainActivity extends AppCompatActivity {
     public void onStart() {
         super.onStart(); // Always call the superclass method first
         //Add code here for onStart callback action
+        Log.d("onStart", "In the onStart event");
     }
 
     @Override
     public void onResume() {
         super.onResume(); // Always call the superclass method first
         //Add code here for onResume callback action
+        Log.d("onResume", "In the onResume event");
     }
 
     @Override
     public void onPause() {
         super.onPause(); // Always call the superclass method first
         //Add code here for onPause callback action
+        Log.d("onPause", "In the onPause event");
     }
 
     @Override
     public void onStop() {
         super.onStop(); // Always call the superclass method first
         //Add code here for onStop callback action
+        Log.d("onStop", "In the onStop event");
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy(); // Always call the superclass method first
         //Add code here for onDestroy callback action
+        Log.d("onDestroy", "In the onDestroy event");
     }
 }
