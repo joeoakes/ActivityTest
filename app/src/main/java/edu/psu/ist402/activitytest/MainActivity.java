53dc304a8bcafde7fb8f11c4424e33242d1a78da
Joe Oakes <joe.oakes@gmail.com>
2017-06-04 22:42:24 -0400

    Add by Joe

--- app/src/main/java/edu/psu/ist402/activitytest/MainActivity.java
+++ app/src/main/java/edu/psu/ist402/activitytest/MainActivity.java
@@ -10,4 +10,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public void onStart() {
+        super.onStart(); // Always call the superclass method first
+        //Add code here for onStart callback action
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume(); // Always call the superclass method first
+        //Add code here for onResume callback action
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause(); // Always call the superclass method first
+        //Add code here for onPause callback action
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop(); // Always call the superclass method first
+        //Add code here for onStop callback action
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy(); // Always call the superclass method first
+        //Add code here for onDestroy callback action
+    }
 }
